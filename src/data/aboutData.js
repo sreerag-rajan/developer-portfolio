586a9bf1c6298ee46b79b158c8819673877d1dc4
Moises De Leon <mdeleon5826@gmail.com>
2022-04-20 12:29:14 -0400

    finalized the last few changes to the app

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "Hello! My name is Moises De Leon. I'm an aspiring web developer based in the US.",
-    description2: "I am a student at DigitalCrafts, currently learning Fullstack Web-Development. After many years working in the Health Industry . During the course of this program i was immersed into many programming languages and libraries such as: HTML, CSS, Node.js, PostgreSQL, MongoDB, React, and Redux. With these knowledge in place, i greatly look forward to working within your team.",
+    description2: "I am a student at DigitalCrafts, currently learning Fullstack Web-Development. During the course of this program I was immersed into many programming languages and libraries such as: HTML, CSS, Node.js, PostgreSQL, MongoDB, React, and Redux. With these knowledge in place, I greatly look forward to working within your team.",
     image: 2
 }
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,10 @@
-import resume from '../assets/pdf/Moises-resume-2022.docx'
+import resume from '../assets/pdf/Moises-resume2022.pdf';
 import Me from '../assets/img/me.jpg'
 
 export const headerData = {
     name: 'Moises De Leon',
     title: "Full Stack Software Developer",
-    desciption:"Strong professional and creative software developer that can pick up new languages and systems quickly and can adapt to changing situations as needed. Previously, I was a Psychology major working directly with patients with psychological disabilities to help them gain independence throughout their daily lives. Now i look to use the interpersonal and technical skills that i've acquired over the years to create reactive applications",
+    desciption:"Strong professional and creative software developer that can pick up new languages and systems quickly and can adapt to changing situations as needed. Previously, I majored in Psychology and worked directly with patients with mental disabilities and helped them gain independence throughout their daily lives. Now i look to use the interpersonal and technical skills that i've acquired over the years to create interactive and stunning applications",
     image: Me,
     resumePdf: resume
 }
