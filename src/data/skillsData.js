1798f18c56354069d76437877fcab0943e3144b4
chuks dozie <chuksdozie48@gmail.com>
2022-07-27 09:48:04 +0100

    most of the chunk

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,17 +1,27 @@
- export const skillsData = [
-    'HTML',
-    'Javascript',
-    'CSS',
-    'React',
-    'Django',
-    'MongoDB',
-    'MySQL',
-    'Python'
-]
+export const skillsData = [
+  "HTML",
+  "Javascript",
+  "Twilio",
+  "CSS",
+  "React",
+  "Jest",
+  "Tailwind",
+  "MongoDB",
+  "Python",
+  "Supertest",
+  "Node JS",
+  "Typescript",
+  "Ubuntu",
+  "Redis",
+  "PostgreSQL",
+  "AWS",
+  "Firebase",
+  "MaterialUI",
+  "React Native",
+];
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
 // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-
 
 // AVAILABLE SKILLS
 
