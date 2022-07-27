5b3277c35811f08b4e1bb1d1287b47b5c15f7701
chuks dozie <chuksdozie48@gmail.com>
2022-07-27 09:59:47 +0100

    skip projects for now

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,28 +1,42 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
+import React from "react";
+import { Helmet } from "react-helmet";
 
-import { Navbar, Footer, Landing, About, Skills, Testimonials, Blog, Education, Experience, Contacts, Projects, Services, Achievement } from '../../components'
-import { headerData } from '../../data/headerData'
+import {
+  Navbar,
+  Footer,
+  Landing,
+  About,
+  Skills,
+  Testimonials,
+  Blog,
+  Education,
+  Experience,
+  Contacts,
+  Projects,
+  Services,
+  Achievement,
+} from "../../components";
+import { headerData } from "../../data/headerData";
 
 function Main() {
-    return (
-        <div>
-            <Helmet>
-                <title>{headerData.name} - Porfolio</title>
-            </Helmet>
+  return (
+    <div>
+      <Helmet>
+        <title>{headerData.name} - Porfolio</title>
+      </Helmet>
 
-            <Navbar />        
-            <Landing />
-            <About />
-            <Skills />
-            <Education />            
-            <Projects />
-            <Achievement />
-            {/* <Blog /> */}
-            <Contacts />
-            {/* <Footer /> */}
-        </div>
-    )
+      <Navbar />
+      <Landing />
+      <About />
+      <Skills />
+      <Education />
+      {/* <Projects /> */}
+      <Achievement />
+      {/* <Blog /> */}
+      <Contacts />
+      {/* <Footer /> */}
+    </div>
+  );
 }
 
-export default Main
+export default Main;
