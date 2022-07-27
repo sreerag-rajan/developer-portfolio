785d0800ce92140b03bcad11175ff14d0a742db4
chuks dozie <chuksdozie48@gmail.com>
2022-07-27 10:51:38 +0100

    description correction

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ export const headerData = {
   name: "Chiedozie Chukwu",
   title: "Fullstack Web & Mobile Developer",
   desciption:
-    "For me, it is all about creating something new and unique - Invention, it is also about maintaining what has been created. That's why I am a developer.",
+    "For me, it is all about creating something new and unique - Invention, it is also about maintaining what has been created. One thing we can never lack in thw world is problems, it is a major part of a developers job to solve problems and that's why I am a developer.",
   image:
     "https://banner2.cleanpng.com/20180623/iqh/kisspng-computer-icons-avatar-social-media-blog-font-aweso-avatar-icon-5b2e99c40ce333.6524068515297806760528.jpg",
   resumePdf: resume,
