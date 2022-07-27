2a24c7f4ed2b47466ab9bc6583b8de9b772efee7
chuks dozie <chuksdozie48@gmail.com>
2022-07-27 10:37:52 +0100

    about me

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,8 @@
 export const aboutData = {
-    title: "Who I am",
-    description1: "Hello! My name is Sreerag Rajan. I'm an aspiring web developer based in India.",
-    description2: "I am a student at Masai School, currently learning Fullstack Web-Development. A project that I worked on towards the end of my Master’s in History set me on this path of a web developer. The project involved looking at movies on historical themes that could be considered as an educational tool. The website was not the focus of the project, but working on it was such a thrilling experience that a career in this felt like a right fit. Other than this I am a bibliophile, who not just loves reading books but also buying and maintaining a personal library.",
-    image: 2
-}
+  title: "Who I am",
+  description1:
+    "Hello! My name is Chiedozie Chukwu. I'm a Fullstack Developer based in Nigeria.",
+  description2:
+    "I create frontend for Web Apps using React mostly and Mobile Apps (ios and android) using React Native. I also create servers using Express and Node Js, depending on the project structure I use Mongo DB or PostgresQL as choice database . I have worked on some individual projects but most especiallyI have collaborated on a good number of team projects, regular stand up meetings and SCRUM reports aids working in a team.\n Today I work as a Fullstack Mobile Developer. I mentor beginner developers physically through Internships and virtually via Tiktok.",
+  image: 2,
+};
